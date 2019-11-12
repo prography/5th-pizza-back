a9d1a299fe7ba98901c5321d00220fefcc45cd55
moonelysian <dandyblack@naver.com>
2019-11-12 14:14:33 +0900

    challenge controller 기본 기능 생성

--- src/controllers/ChallengeController.js
+++ src/controllers/ChallengeController.js
@@ -2,20 +2,39 @@ const sequelize = require('sequelize')
 const models = require('../models')
 const Op = sequelize.Op;
 
-const getChallenges = function(req ,res){
-    
+const getChallenges =  function(req ,res){
+    models.Challenges.findAll()
+    .then( result => res.send({code: 200, challenges: challenges}))
 }
 
 const getChallenge = function(req, res){
-    
+    const id = req.params.challenges
+    models.Challenges.findOne({where: {id: id}})
+    .then(result => res.send({code: 200, challenge: result}))
 }
 
 const createChallenge = function(req, res){
-
+    const body = req.body
+    const challenge = {
+        routine_type: body.routine_type,
+        object_unit: body.object_unit,
+        quota: body.quota,
+        exercise_type: body.exercise_type
+    }
+    
+    try{
+        models.Challenges.create(challenge)
+        .then(result => res.send({ code:200 }))
+    }
+    catch(err){
+        console.log(err)
+    }
 }
 
 const deleteChallenge = function(req, res){
-
+    const id = req.params.challenge
+    models.Challenges.destroy({where: {id: id}})
+    .then(result => res.send({code: 200}))
 }
 
 
--- src/models/Challenges.js
+++ src/models/Challenges.js
@@ -3,7 +3,8 @@ module.exports = (sequelize, DataTypes) => {
   const challenges = sequelize.define('Challenges', {
     routine_type: DataTypes.ENUM('daily','weekly','monthly'),
     object_unit: DataTypes.ENUM('distance','time'),
-    quota: DataTypes.DOUBLE
+    quota: DataTypes.DOUBLE,
+    exercise_type: DataTypes.ENUM('running', 'cycling')
   }, {
       timestamp: true
   });
