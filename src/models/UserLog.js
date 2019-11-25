4b79acc7ee091c0c25a27a25f6a334807565e6c2
moonelysian <dandyblack@naver.com>
2019-11-25 14:06:08 +0900

    user 모델, 로그인 로그아웃 로그 모델

--- src/models/UserLog.js
+++ src/models/UserLog.js
@@ -1,15 +1,15 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const user_log = sequelize.define('UserLog', {
-    // user_no: DataTypes.STRING,
-    // log_in: DataTypes.DATETIME,
-    // log_out: DataTypes.DATETIME
+    user_no: DataTypes.STRING,
+    log_in: DataTypes.DATE,
+    log_out: DataTypes.DATE
   });
   
   user_log.associate = function(models) {
-    // user_log.belongsTo( models.Users,{
-    //     foreignKey: 'user_no'
-    // })
+    user_log.belongsTo( models.Users,{
+        foreignKey: 'user_no'
+    })
   };
   return user_log;
 };
