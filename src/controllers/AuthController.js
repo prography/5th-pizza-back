dd2615ffa0235c5041a7eccbfe96bdc133fc162e
moonelysian <dandyblack@naver.com>
2019-11-25 14:04:55 +0900

    로그인 로그아웃 부분

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -7,17 +7,38 @@ const secretObj = require("../config/token");
 const Op = sequelize.Op;
 
 const login = async function(req, res){
-    const user_id = req.body.user_id
-    const user_pw = req.body.user_pw
+    const kakao_token = res.body.access_token
+    const user = {
+        user_id: req.body.user_id,
+        user_pw: req.body.user_pw
+    }
+    
+    if (kakao_token) {
+        const result = await models.Users.findOne({ where: { [Op.and]: [{user_id: user.user_id} , {password: user.user_pw}] } })
 
-    const result = models.Users.findOne({ where: { [Op.and]: [{user_id: user_id} , {password: user_pw}] } })
-    if (result) {
-        const token = jwt.sign({ user_id: user_id }, secretObj, { expiresIn: '7d' })       
-    }     
+        if (result) {
+            const token = jwt.sign({ user_id: user_id }, secretObj, { expiresIn: '7d' })
+            const log = {
+                user_no: result.id,
+                log_in: moment()
+            }
+            res.cookie({user_id: user.user_id}, token)
+            res.send({ data: result })
+
+        }
+        else {
+            throw new Error('Unverified user')
+        }
+    }
+    else {
+        throw new Error('kakao access token error')
+    }
+    
 }
 
 const logout = function(req, res){
-
+    const user_id = req.body.id
+    res.clearCookie({user_id: user_id})
 }
 
 module.exports = {
