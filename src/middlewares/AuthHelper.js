91a2dce2659fad376104c3cc628f3f0b353ad524
moonelysian <dandyblack@naver.com>
2019-11-26 23:45:09 +0900

    req.user에 user정보 넣기

--- src/middlewares/AuthHelper.js
+++ src/middlewares/AuthHelper.js
@@ -1,24 +1,22 @@
 const models = require('../models')
 const jwt = require('jsonwebtoken')
-const secretObj = require("../config/key");
 
 const verifyToken = async function(req, res, next ){
     const token = req.headers['x-access-token']
     if (!token) {
         res.status(401).send( { error: 'Not logged in' } )
-        next();
     }
 
     else {
-        const decoded = jwt.verify(token , secretObj)
+        const decoded = jwt.verify(token , process.env.PASSWORD_SECRET)
         const user = decoded.user_id
         const isExist = await models.Users.findOne({ where: {user_id: user} })
         if (isExist) {
+            req.user(user)
             next();
         }
         else {
             res.status(401).send( { error: 'Not logged in' } );
-            next();
         }
     }
 }
