a568e38f852afd42728726d6436e300ad7a704ba
moonelysian <dandyblack@naver.com>
2019-11-28 00:47:25 +0900

    jwt sign data 변수명 오류 변경

--- src/middlewares/AuthHelper.js
+++ src/middlewares/AuthHelper.js
@@ -3,13 +3,14 @@ const jwt = require('jsonwebtoken')
 
 const verifyToken = async function(req, res, next ){
     const token = req.headers['x-access-token']
+    
     if (!token) {
         res.status(401).send( { error: 'Not logged in' } )
     }
 
     else {
         const decoded = jwt.verify(token , process.env.PASSWORD_SECRET)
-        const user_id = decoded.user_id
+        const user_id = decoded.user
         const user  = await models.Users.findOne({ where: {user_id: user_id} })
         if (user) {
             req.user = user
