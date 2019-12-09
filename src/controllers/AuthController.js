b0d6600627c9946f6356aea214adefc630bbeaa7
moonelysian <dandyblack@naver.com>
2019-12-09 22:45:58 +0900

    함수명 login -> kakaologin

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -3,26 +3,28 @@ const moment = require('moment')
 const jwt = require('jsonwebtoken')
 const axios = require('axios')
 
-const login = async function(req, res){
-    const access_token = req.body.x_kakao_token
-    console.log(access_token)
+const kakaoLogin = async function(req, res){
+    const access_token = req.headers['x-kakao_token']
     
     if (access_token) {
         const userInfo = await getUserInfo(access_token)
-        console.log(userInfo.properties.nickname)
+        const serviceAccount = {
+            type: 'kakao',
+            token: access_token,
+            user_id: userInfo.id,
+            created_at: moment()
+        }
         const isExist = await models.Users.findOne({ where: { user_id: userInfo.id } })
-        const token = jwt.sign( { user_id: userInfo.id }, process.env.PASSWORD_SECRET , { expiresIn: '7d' })
+        const token = jwt.sign({ user_id: userInfo.id }, process.env.PASSWORD_SECRET , { expiresIn: '7d' })
 
-        if (isExist) { res.send( { data: isExist, access_token: token } ) }
+        if (isExist) { res.send({ data: isExist, access_token: token }) }
         else {
             const user = {
-                user_id: userInfo.id,
                 email: userInfo.kakao_account.email,
                 nickname: userInfo.properties.nickname,
-                create_at: moment()
+                created_at: moment()
             }
             const result = await models.Users.create(user)
-
             if (result) { res.send({ data: result, access_token: token }) }
             else { throw new Error('Cannot create user') }
         }
@@ -45,5 +47,5 @@ const getUserInfo = async function(access_token) {
 }
 
 module.exports = {
-    login
+    kakaoLogin
 }
