6d468520cf54c325bafe75f49f4449e146f65b4b
moonelysian <dandyblack@naver.com>
2019-12-12 10:54:30 +0900

    where 절 변경

--- src/middlewares/VerifyToken.js
+++ src/middlewares/VerifyToken.js
@@ -4,14 +4,13 @@ const jwt = require('jsonwebtoken')
 const verifyToken = async function(req, res, next ){
     const token = req.headers['x-access-token']
     if (!token) {
-        res.status(401).send({ error: 'Not logged in' })
+        res.status(401).send({ error: 'token err' })
     }
 
     else {
         const decoded = jwt.verify(token , process.env.PASSWORD_SECRET)
-        const user_id = decoded.user
-        console.log(user_id)
-        const user  = await models.Users.findOne({ where: { user_id: user_id } })
+        const user_id = decoded.user_id
+        const user  = await models.Users.findOne({ where: { id: user_id } })
         if (user) {
             req.user = user
             next();
