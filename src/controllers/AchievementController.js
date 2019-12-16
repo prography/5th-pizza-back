906593a5c7a4ed75a80169531b5c835883ef0cca
moonelysian <dandyblack@naver.com>
2019-12-16 22:31:33 +0900

    챌린지 생성 날짜부터 오늘까지의 기록 결과 합하기

--- src/controllers/AchievementController.js
+++ src/controllers/AchievementController.js
@@ -3,52 +3,71 @@ const models = require('../models')
 const moment = require('moment')
 const Op = sequelize.Op;
 
-const setAchievement = function (req, res){
-    const body = req.body
-    const challenge_id = body.challege_id
-    const challenge = await models.Challenges.findOne({ where: { id: challenge_id }})
+const setAchievement = function (challenge, user) {
     if (challenge.routine_type == 'daily') {
-        setDailyAchievement(challenge, record)
+        setDailyAchievement(challenge, user)
     }
     else if (challenge.routine_type == 'weekly') {
-        setWeeklyAchievement(challenge, record)
+        setWeeklyAchievement(challenge, user)
     }
     else if (challenge.routine_type == 'monthly') {
-        setMonthlyAchievement(challenge, record)
+        setMonthlyAchievement(challenge, user)
+    }
+    else {
+        throw new Error('Wrong Challenge')
     }
 }
 
 const setDailyAchievement = function(challenge, record){
     if (challenge.object_unit == 'time') {
-        if (record.running_time >= quota) {
-            
+        if (record.running_time >= challenge.quota) {
+            return { achievement: 100 }
         }
+        else return { achievement: 0 }
     }
     else {
-        if (record.distance >= quota){
-
+        if (record.distance >= challenge.quota){
+            return { achievement: 100 }
         }
+        else return { achievement: 0 }
     }
 }
 
-const setWeeklyAchievement = function(challenge, record){
+const setWeeklyAchievement = function(challenge, user){
+    const total = 0
     if (challenge.object_unit == 'time') {
-        const total = await model.Records.sum('running_time', { where: { challege_id: challenge.id }})
-        const achievement = (total / challenge.quota)*100
-        return achievement
+        total = await models.Records.sum('running_time', 
+        { where: 
+            {[Op.and]: [
+                { challege_id: challenge.id }, 
+                { user_id: user.id }, 
+                { created_at: {[Op.between]: [ challenge.created_at, moment()]}}
+            ]}
+        })
     }
+    else if (challenge.object_unit == 'distance') {
+        total = await models.Records.sum('distance', 
+        { where: 
+            {[Op.and]: [
+                { challege_id: challenge.id }, 
+                { user_id: user.id }, 
+                { created_at: {[Op.between]: [ challenge.created_at, moment()]}}
+            ]}
+        })
+    }
+    else {
+        throw new Error('Wrong object unit')
+    }
+    const achievement = (total / challenge.quota) * 100
+    return { achievement: achievement }
 }
 
 const setMonthlyAchievement = function(challenge, record){
     if (challenge.object_unit == 'time') {
-        if (record.running_time >= quota){
-            
-        }
-        else {}
+
     }
     else {
-        if (record.distance >= quota) {
-        }
-        else {}
     }
 }
+
+module.exports = { setAchievement }
