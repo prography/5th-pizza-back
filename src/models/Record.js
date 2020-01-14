f4374c36e31e64d0fc44ad0b0e0015b74ace459d
moonelysian <dandyblack@naver.com>
2020-01-14 11:38:29 +0900

    screenshot 컬럼 타입 json -> string

--- src/models/Record.js
+++ src/models/Record.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
     running_time: DataTypes.DOUBLE,
     distance: DataTypes.DOUBLE,
     created_at: DataTypes.DATE,
-    screenshot: DataTypes.JSON
+    screenshot: DataTypes.STRING
   }, 
   {
     timestamps: false
