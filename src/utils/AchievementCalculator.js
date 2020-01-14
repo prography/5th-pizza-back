f30f0500e89ce5726aaf6d2acf7d6d5d32d9c3a0
moonelysian <dandyblack@naver.com>
2020-01-14 11:37:52 +0900

    달성률 계산 object_type -> object_unit 으로 오타 변경

--- src/utils/AchievementCalculator.js
+++ src/utils/AchievementCalculator.js
@@ -4,7 +4,7 @@ const getAchievement = async function (challenge, user) {
     const records = await models.Records.findAll({ where: { user_id: user.id, challenge_id: challenge.id } })
     let total = 0;
     let goal = challenge.quota;
-    if (challenge.object_type === 'time') {
+    if (challenge.object_unit === 'time') {
         total = records.reduce((acc, record) => acc + record.running_time, 0);
     } else {
         total = records.reduce((acc, record) => acc + record.distance, 0)
