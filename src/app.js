1763a98bcfc1214d3b75816d0e49e2323f31bbd2
geusan <dnay2k@gmail.com>
2020-01-24 16:25:15 +0900

    database 로딩 로직 수정

--- src/app.js
+++ src/app.js
@@ -1,15 +1,12 @@
 import express from 'express';
 import routes from './routes';
 import bodyParser from 'body-parser';
-import { sequelize } from './models';
 
 const app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-
-sequelize.sync();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import app from './app';
+import { sequelize } from './models';
 
 const port = process.env.PORT || 4000;
 const host = process.env.HOST || 'localhost';
+async function main() {
+  await sequelize.sync()
+  app.listen(port, host, () => {
+    console.log(`server is running on ${port}`)
+  });
+}
 
-app.listen(port, host, () => {
-  console.log(`server is running on ${port}`)
-});
+main();
