d027ae0ec77d58de8b4cf94711f0e97b3d3fc33f
moonelysian <dandyblack@naver.com>
2020-01-31 16:10:02 +0900

    objectUnit undefined 오류 해결

--- src/utils/AchievementCalculator.js
+++ src/utils/AchievementCalculator.js
@@ -2,9 +2,10 @@ import { Record } from '../models'
 
 const getAchievement = async function (challenge, user) {
     const records = await Record.findAll({ where: { userId: user.id, challengeId: challenge.id } })
+    const baseChallenge = await challenge.getBaseChallenge();
     let total = 0;
-    let goal = (await challenge.getBaseChallenge()).quota;
-    if (challenge.objectUnit === 'time') {
+    let goal = baseChallenge.quota;
+    if (baseChallenge.objectUnit === 'time') {
         total = records.reduce((acc, record) => acc + record.runningTime, 0);
     } else {
         total = records.reduce((acc, record) => acc + record.distance, 0)
